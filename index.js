b6bdd7c7280d28db4bdaf1230e4de0e7dcd18ee2
Reid Barnowitz <rbarn2731@gmail.com>
2023-03-19 20:40:05 -0400

    my completed lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.getElementById('main').remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "DONKEY KONG is the champion"
